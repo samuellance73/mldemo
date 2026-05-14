f4984a63eb772379eb29e8a2ec0a85e5ea6f6f45
Fang <SamuelFang263@users.noreply.huggingface.co>
2026-05-14 23:28:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 
 echo "Starting Tailscale Daemon in userspace mode..."
 # We must use userspace-networking because HF doesn't allow root TUN devices
-tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --ssh &
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055&
 
 # Wait 3 seconds for the daemon to wake up
 sleep 3
 
 echo "Connecting to Tailnet..."
 # Log in using the secret key we saved in HF Settings
-tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="hf-ubuntu-server"
+tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="hf-ubuntu-server"  --ssh 
 
 echo "Starting File Browser..."
 # Start File Browser on port 7860 (HF's default port)
