a38f8184dfe017144740e459d375397c640c2818
TrueKing <samuellance73@gmail.com>
2026-08-09 00:28:45 +0000

    Remove node_modules from git index and update .gitignore

--- node_modules/@mercuryworkshop/scramjet-utils/dist/types/alwaysLastBubble.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ScramjetClient } from "@mercuryworkshop/scramjet";
-export declare function setupAlwaysLastBubble(client: ScramjetClient, whatToCapture: string[]): <T extends Event>(target: EventTarget, eventName: string, listener: (e: T) => void) => void;
-export type AddAlwaysLastEventListener = ReturnType<typeof setupAlwaysLastBubble>;
